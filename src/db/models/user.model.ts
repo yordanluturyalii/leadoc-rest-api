d26f9601caafc5d4f38701eecb2dcd177fd551fc
yordanluturyalii <yordanluturyali@gmail.com>
2025-06-26 06:58:29 +0700

    :card_file_box: refactor: update field in model

--- src/db/models/user.model.ts
+++ src/db/models/user.model.ts
@@ -2,8 +2,11 @@ export class User {
   constructor(
     public id: string | undefined,
     public name: string | undefined,
+    public email: string | undefined,
     public username: string | undefined,
     public profile_picture: string | null | undefined,
     public github_id: string | undefined,
+    public password: string | undefined,
+    public accessToken: string | undefined
   ) {}
 }
