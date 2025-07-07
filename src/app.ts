a7a088f2512c1b98da3cfcc51cb0133afdf8a859
yordanluturyalii <yordanluturyali@gmail.com>
2025-07-07 22:46:01 +0700

    :wastebasket: clean up code

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import { errorHandler } from "./exceptions/error_handler";
 import cors from "cors";
 import { limiter } from "./utils/rate-limiting.utils";
 import "reflect-metadata";
-import routes from "./routes/coba.routes";
 
 const app = express();
 
@@ -11,7 +10,6 @@ app.use(cors());
 app.use(limiter);
 app.use(express.json());
 
-app.use("/", routes);
 
 app.use(errorHandler);
 export { app }
