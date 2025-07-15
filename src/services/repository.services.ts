824b5ca642bbe6d35aaf0345d31bf2084c48e3b0
yordanluturyalii <yordanluturyali@gmail.com>
2025-07-15 21:54:25 +0700

    :sparkles: feat: add service for get access token

--- src/services/repository.services.ts
+++ src/services/repository.services.ts
@@ -5,5 +5,12 @@ import type { UserRepository } from "../repositories/user.repository";
 export class RepositoryServices {
   constructor(@Inject() private userRepository: UserRepository){}
 
-  
+  async getAccessToken(userId: string) {
+    try {
+      const user = await this.userRepository.findById(userId);
+      return user?.accessToken;
+    } catch (error) {
+      return error;
+    }
+  }  
 }
