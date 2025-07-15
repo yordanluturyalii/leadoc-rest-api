d2542bee9ee709621624bde18e2d75546fc85f0b
el <eldedestroyer@gmail.com>
2025-07-15 18:52:13 +0700

    ♻️ refactor: change login validation

--- src/validations/login.validation.ts
+++ src/validations/login.validation.ts
@@ -1,22 +1,24 @@
 import { body } from "express-validator";
 import Container from "typedi";
 import { UserRepository } from "../repositories/user.repository";
+import bcrypt from "bcryptjs";
 
 const userRepository = Container.get(UserRepository);
 
-export const registerValidator = [
+export const loginValidator = [
     body("email", "Invalid Email").isEmail(),
     body("email", "Email is required").not().isEmpty(),
     body("email").custom(async (value) => {
         const existingEmail = await userRepository.findByEmail(value);
-        if (existingEmail) throw new Error("This email must be in the correct format and not linked to an existing user.");
+        if (!existingEmail) throw new Error("This email must be in the correct format and linked to an existing user.");
     }),
     body("password_confirmation", "Password confirmation is required").not().isEmpty(),
     body('password_confirmation', 'The minimum password confirmation length is 8').isLength({ min: 8 }),
     body('password', 'The minimum password length is 8').isLength({ min: 8 }),
     body('password', 'Password is required').not().isEmpty(),
-    body('password').custom((value, { req }) => {
-        if (value !== req.body.password_confirmation) throw new Error("The password confirmation does not match.");
-        return true;
+    body("password").custom(async (value, {req}) => {
+        const passwordHash = await userRepository.findByEmail(req.body.email);
+        const isMatch = await bcrypt.compare(value, passwordHash?.password)
+        if (!isMatch) throw new Error("Password Incorrect");
     }),
 ];
