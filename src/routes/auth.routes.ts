78083ec96be6515772c65f85fd1cde5154b963e2
el <eldedestroyer@gmail.com>
2025-07-16 11:59:44 +0700

    ✨ feat: add new routes for get me user

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,6 +5,7 @@ import { AuthController } from "../controllers/auth.controllers";
 import { Router, type Request, type Response } from "express";
 import { registerValidator } from "../validations/register.validation";
 import { loginValidator } from "../validations/login.validation";
+import authMiddleware from "../middlewares/auth.middlewares";
 
 setupDI();
 const authRoutes = Router();
@@ -25,5 +26,7 @@ authRoutes.get(
 
 authRoutes.post("/auth/register", registerValidator, (req: Request, res: Response) => authController.register(req, res));
 authRoutes.post("/auth/login", loginValidator, (req: Request, res: Response) => authController.login(req, res));
+
+authRoutes.get("/auth/me", authMiddleware, (req: Request, res: Response) => authController.getMe(req, res));
 
 export default authRoutes;
