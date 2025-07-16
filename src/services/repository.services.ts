438f60aa8ebe26e9bc1e6aaf043709c281cac117
yordanluturyalii <yordanluturyali@gmail.com>
2025-07-16 23:03:07 +0700

    :necktie: feat: add logic for get repository user

--- src/services/repository.services.ts
+++ src/services/repository.services.ts
@@ -1,5 +1,8 @@
 import { Inject, Service } from "typedi";
 import type { UserRepository } from "../repositories/user.repository";
+import { Octokit } from "octokit";
+import { logger } from "../utils/logger.utils";
+import { name } from "drizzle-orm";
 
 @Service()
 export class RepositoryServices {
@@ -22,4 +25,43 @@ export class RepositoryServices {
       return error;
     }
   }  
+
+
+  async getRepo(accessToken: string, githubId: string) {
+    const octokit = new Octokit({
+      auth: accessToken
+    });
+
+    const {data: repositories} = await octokit.rest.repos.listForAuthenticatedUser({
+      per_page: 100
+    }); 
+    
+    const user = await this.userRepository.findByGithubId(githubId);
+    
+    const ownRepositories = repositories.filter(repo => repo.owner.login === user?.username);
+
+    const userRepositories = await Promise.all( 
+      ownRepositories.map(async(item, index)=> {
+      let haveReadme = false;
+
+      try {
+        await octokit.rest.repos.getReadme({
+          owner: item.owner.login,
+          repo: item.name 
+        }); 
+        haveReadme = true
+      } catch (error) {
+        logger.error("Error: %o", error?.message);
+      }
+
+      return {
+        id: index + 1,
+        name: item.full_name,
+        isPrivate: item.private,
+        haveReadme
+      }
+    }));
+
+    return userRepositories;
+  }
 }
