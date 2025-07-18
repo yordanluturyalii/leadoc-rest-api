a85472b3918de4a06242c3648c8ec2a0d682650d
yordanluturyalii <yordanluturyali@gmail.com>
2025-07-19 00:35:53 +0700

    :card_file_box: feat: add function to save repository

---
+++ src/repositories/repo.repository.ts
@@ -0,0 +1,19 @@
+import { db } from "../db/db";
+import { Repository } from "../db/models/repositories.model";
+import { repositories } from "../db/schema";
+
+export class RepoRepository {
+  async create(name: string, visibility: "PRIVATE" | "PUBLIC", userId: string, createdAt: Date, updatedAt: Date | null) {
+    const repo = await db.insert(repositories).values({
+      name, visibility, user_id: userId, created_at: createdAt, updated_at: updatedAt
+    }).returning();
+    return repo[0] ? Repository(
+      repo[0].id,
+      repo[0].name,
+      repo[0].visibility,
+      repo[0].user_id,
+      repo[0].created_at,
+      repo[0].updated_at
+    ) : null
+  }
+}
