c5f40aa149f7e26d8a79401eaa2f0f6557fa92d5
yordanluturyalii <yordanluturyali@gmail.com>
2025-07-19 00:51:55 +0700

    :card_file_box: feat: add funciton to save many

--- src/repositories/repo.repository.ts
+++ src/repositories/repo.repository.ts
@@ -7,13 +7,28 @@ export class RepoRepository {
     const repo = await db.insert(repositories).values({
       name, visibility, user_id: userId, created_at: createdAt, updated_at: updatedAt
     }).returning();
-    return repo[0] ? Repository(
+    return repo[0] ? new Repository(
       repo[0].id,
       repo[0].name,
       repo[0].visibility,
       repo[0].user_id,
+      repo[0].haveReadme
       repo[0].created_at,
       repo[0].updated_at
     ) : null
+  }
+
+  async saveMany(entities: Repository[]) {
+    const values = entities.map(repo => ({
+      id: repo.id,
+      name: repo.name,
+      visibility: repo.visibility ? "PRIVATE" : "PUBLIC",
+      haveReadme: repo.haveReadme,
+      user_id: repo.userId,
+      created_at: repo.createdAt,
+      updatedAt: repo.updatedAt
+    }));
+
+    await db.insert(repositories).values(values).onConflictDoNothing(); 
   }
 }
